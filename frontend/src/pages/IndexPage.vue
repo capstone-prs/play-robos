11e7465581522397fc0222a6cfd40093a1a4c4db
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-01 01:52:06 +0800

    Temporarily remove homepage from index to cater login/signup

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
-    <HomePage />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HomePage from './HomePage.vue';
 
 export default defineComponent({
     name: 'IndexPage',
-    components: { HomePage }
 });
 </script>
