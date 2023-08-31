781cfc268988cfd6786e7e0c2f84d6ebb55e5cf3
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-01 01:48:01 +0800

    Remove layout to cater quasar layout in other components

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,13 +1,9 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+  <router-view />
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 
 export default defineComponent({
