e83746a3266e2d805081afd080eae58096501d1f
iforgot-my-username <garibay2018paul@gmail.com>
2023-08-31 11:38:55 +0800

    🚧 Inital Singup and Login Function

--- frontend/src/firebase/auth.ts
+++ frontend/src/firebase/auth.ts
@@ -1,75 +1,15 @@
-import '../boot/firebase';
 import {
   getAuth,
-  signInWithPopup,
-  GoogleAuthProvider,
-  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from 'firebase/auth';
 
-const provider = new GoogleAuthProvider();
 const auth = getAuth();
 
-const login = () => signInWithPopup(auth, provider);
+export const signup = (email: string, password: string) =>
+  createUserWithEmailAndPassword(auth, email, password);
 
-// const login = () => signInWithRedirect(auth, provider);
-export const looOut = () => signOut(auth);
+const login = (email: string, password: string) =>
+  signInWithEmailAndPassword(auth, email, password);
+
 export default login;
-////////////////////////////////
-// import {
-//   getAuth,
-//   signInWithRedirect,
-//   getRedirectResult,
-//   GoogleAuthProvider,
-// } from 'firebase/auth/cordova';
-
-// const auth = getAuth();
-// const login = () =>
-//   signInWithRedirect(auth, new GoogleAuthProvider())
-//     .then(() => {
-//       return getRedirectResult(auth);
-//     })
-//     .then((result) => {
-//       const credential = GoogleAuthProvider.credentialFromResult(result!);
-
-//       const token = credential?.accessToken;
-
-//       // The signed-in user info.
-//       const user = result?.user;
-//       // ...
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       // Handle Errors here.
-//       const errorCode = error.code;
-//       const errorMessage = error.message;
-//     });
-
-/// export default login;
-// import {
-//   getAuth,
-//   getRedirectResult,
-//   GoogleAuthProvider,
-// } from 'firebase/auth/cordova';
-
-// const auth = getAuth();
-// const login = () =>
-//   getRedirectResult(auth)
-//     .then((result) => {
-//       const credential = GoogleAuthProvider.credentialFromResult(result!);
-//       if (credential) {
-//         // This gives you a Google Access Token.
-//         // You can use it to access the Google API.
-//         const token = credential.accessToken;
-//         // The signed-in user info.
-//         const user = result?.user;
-//         // ...
-//       }
-//     })
-//     .catch((error) => {
-//       // Handle Errors here
-//       console.log(error);
-//       const errorCode = error.code;
-//       const errorMessage = error.message;
-//     });
-
-// export default login;
--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-   <button @click="login">Sign In with Google</button>
- </div>
+    <!-- <button @click="login">Sign In with Google</button> -->
+    <SignupForm />
+    <LoginForm />
+  </div>
 </template>
 
 <script lang="ts">
-import '../firebase/config'
-import login from '../firebase/auth'
+// import '../firebase/config'
+import SignupForm from '../components/login/SignupForm.vue';
+import LoginForm from '../components/login/LoginForm.vue';
 import { defineComponent } from 'vue';
 export default defineComponent({
- name: 'IndexPage',
-
-methods: {
- login
-}
+  name: 'IndexPage',
+  components: {
+    SignupForm,
+    LoginForm,
+  },
+  methods: {},
 });
 </script>
-
