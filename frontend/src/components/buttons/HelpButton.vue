bcf2ea700aa2a249b8b6a86125a5d8e947842146
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-01 01:42:50 +0800

    Add reusable button components

---
+++ frontend/src/components/buttons/HelpButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <q-btn round>
+      <q-icon name="img:/help-btn.png" size="xl" />
+    </q-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelpButton',
+});
+</script>
+
