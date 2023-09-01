1bd1fd750390204514f6324ead0a4a8d77316f2c
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-01 22:25:03 +0800

    Add reusable action button

---
+++ frontend/src/components/buttons/ActionButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <q-btn class="glossy hitchcut" rounded color="yellow" text-color="blue">
+      {{ textLabel }}
+    </q-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ActionButton',
+  props: {
+    'textLabel': String
+  },
+  setup(props) {
+    // const textLabel = props['text-label'];
+    return props['textLabel']
+  },
+})
+
+</script>
+
+<style>
+  @font-face {
+    font-family: hitchcut ;
+    src: url('../../../public/./fonts/Hitchcut-Regular.woff');
+  }
+
+  .hitchcut {
+    font-family: 'hitchcut';
+  }
+</style>
