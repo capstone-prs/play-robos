7a76b5c1cb1480aabfbd247ce2ccbeb8e305e3b3
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-02 17:33:33 +0800

    Add studio page with blockly workspace

---
+++ frontend/src/pages/StudioPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <BlocklyComponent
+      id="blockly2"
+      :options="options"
+      ref="foo"
+    ></BlocklyComponent>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import BlocklyComponent from '../components/blockly/BlocklyComponent.vue';
+
+const foo = ref();
+const options = {
+  media: 'media/',
+  grid: {
+    spacing: 25,
+    length: 3,
+    colour: '#ccc',
+    snap: true,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+html,
+body {
+  margin: 0;
+}
+
+#blockly2 {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
