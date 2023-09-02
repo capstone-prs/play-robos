8bbcfe34b1d3d5b0c978bf1591bb66f46d7520a3
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-02 22:41:32 +0800

    Add buttons as overlay to blockly workspace

--- frontend/src/components/blockly/BlocklyComponent.vue
+++ frontend/src/components/blockly/BlocklyComponent.vue
@@ -1,5 +1,24 @@
 <template>
-  <div ref="blocklyContainer"></div>
+  <div class="workspace-container">
+
+    <div class="overlay-container">
+      <div class="row">
+        <div class="col-6 check">
+          <ActionButton text-label="CHECK" />
+        </div>
+        <div class="col-3 buttons">
+          <HelpButton />
+        </div>
+        <div class="col-3 buttons">
+          <MenuButton />
+        </div>
+      </div>
+    </div>
+
+    <div ref="blocklyContainer" class="blockly-container"></div>
+
+  </div>
+
 </template>
 
 <script lang="ts">
@@ -7,52 +26,74 @@ import { defineComponent, ref, onMounted } from 'vue';
 import Blockly from 'blockly';
 import './blocks/stocks'
 import { level1 } from './toolbox/toolbox'
+import MenuButton from '../buttons/MenuButton.vue';
+import HelpButton from '../buttons/HelpButton.vue';
+import ActionButton from '../buttons/ActionButton.vue';
 
 export default defineComponent({
-  name: 'BlocklyComponent',
-  setup() {
-    const blocklyContainer = ref<string | Element>('');
-
-    onMounted(() => {
-      Blockly.inject(blocklyContainer.value, {
-        // refer to toolbox.js file, we can define more levels from there,
-        // future handling may be passing the level number as props to this component
-        toolbox: level1,
-        trashcan: true,
-        grid:
-        {
-          spacing: 20,
-          length: 3,
-          colour: '#ccc',
-          snap: true
-        },
-         zoom: {
-          startScale: 1.5,
-          maxScale: 3,
-          minScale: 0.3,
-          scaleSpeed: 1.2,
-        },
-        theme: {
-          name: 'custom',
-          'componentStyles': {
-            'workspaceBackgroundColour': '#FFFFFF',
-            'flyoutBackgroundColour': '#D0D0D0',
-            'flyoutOpacity': 0.7,
-          },
-        }
-      });
-    });
-
-    return {
-      blocklyContainer,
-    };
-  },
+    name: 'BlocklyComponent',
+    setup() {
+        const blocklyContainer = ref<string | Element>('');
+        onMounted(() => {
+            Blockly.inject(blocklyContainer.value, {
+                // refer to toolbox.js file, we can define more levels from there,
+                // future handling may be passing the level number as props to this component
+                toolbox: level1,
+                trashcan: true,
+                grid: {
+                    spacing: 20,
+                    length: 3,
+                    colour: '#ccc',
+                    snap: true
+                },
+                zoom: {
+                    startScale: 1.5,
+                    maxScale: 3,
+                    minScale: 0.3,
+                    scaleSpeed: 1.2,
+                },
+                theme: {
+                    name: 'custom',
+                    'componentStyles': {
+                        'workspaceBackgroundColour': '#FFFFFF',
+                        'flyoutBackgroundColour': '#D0D0D0',
+                        'flyoutOpacity': 0.7,
+                    },
+                }
+            });
+        });
+        return {
+            blocklyContainer,
+        };
+    },
+    components: { MenuButton, HelpButton, ActionButton }
 });
 </script>
 
 <style>
-.blocklyToolboxCategory {
-  background-color: rgb(210, 255, 255);
+.workspace-container {
+  position: relative;
+  height: 100vh;
+}
+
+.blockly-container {
+  height: 100%;
+  z-index: 1;
+}
+
+.overlay-container {
+  position: absolute;
+  z-index: 2;
+  padding-top: 5px;
+  left: 73%;
+}
+
+.buttons {
+  padding: 2px;
+}
+
+.check {
+  padding: 5px;
 }
 </style>
 
