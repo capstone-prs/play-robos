da297b17db086485e418cceeae3a12f7544a4a5a
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-02 19:57:09 +0800

    Add toolbox for customizing toolbox based on different levels

---
+++ frontend/src/components/blockly/toolbox/toolbox.js
@@ -0,0 +1,17 @@
+export const level1 = {
+          'kind': 'flyoutToolbox',
+          'contents': [
+            {
+              'kind': 'block',
+              'type': 'wink'
+            },
+            {
+              'kind': 'block',
+              'type': 'blink'
+            },
+              {
+              'kind': 'block',
+              'type': 'eye_roll'
+            }
+          ]
+        }
