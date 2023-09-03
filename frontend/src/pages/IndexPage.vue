61d8b7179812c4771f76d8ebba195e073995201e
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-03 15:34:52 +0800

    Modify index to display studio page  on app launch

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
     <!-- <button @click="login">Sign In with Google</button> -->
-    <SignupForm />
-    <LoginForm />
+    <!-- <SignupForm />
+    <LoginForm /> -->
+    <StudioPage />
   </div>
 </template>
 
 <script lang="ts">
 // import '../firebase/config'
-import SignupForm from '../components/login/SignupForm.vue';
-import LoginForm from '../components/login/LoginForm.vue';
+// import SignupForm from '../components/login/SignupForm.vue';
+// import LoginForm from '../components/login/LoginForm.vue';
 import { defineComponent } from 'vue';
+import StudioPage from './StudioPage.vue';
 export default defineComponent({
   name: 'IndexPage',
   components: {
-    SignupForm,
-    LoginForm,
-  },
+    StudioPage
+},
   methods: {},
 });
 </script>
