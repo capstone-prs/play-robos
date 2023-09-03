d4fb458a008d0a4fac6dbdd5d749aafef12f5819
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-03 22:09:23 +0800

    Add toggle butotn for switching between age groups

---
+++ frontend/src/components/buttons/ToggleButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-btn-toggle
+  size="lg"
+  class="toggle-btn"
+  v-model="model"
+  glossy
+  rounded
+  toggle-color="amber"
+   :options="[
+     { label: '5-7', value: '5-7', slot: 'one' },
+     { label: '8-11', value: '8-11', slot: 'two' },
+   ]"
+  >
+  <template v-slot:one />
+  <template v-slot:two />
+
+  </q-btn-toggle>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'ToggleButton',
+  setup() {
+    return {
+      model: ref('5-7')
+    }
+  }
+})
+</script>
+
+<style>
+@font-face {
+  font-family: hitchcut;
+  src: url('/fonts/Hitchcut-Regular.woff');
+}
+
+.toggle-btn {
+  font-family: 'hitchcut';
+}
+</style>
