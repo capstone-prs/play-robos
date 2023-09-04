9539f71f315655a4d196f6a66cc89a3a9922c63b
RenzoLaporno <renzogabriel.laporno-19@cpu.edu.ph>
2023-09-04 21:39:03 +0800

    Auth Page

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
     <!-- <button @click="login">Sign In with Google</button> -->
-    <SignupForm />
-    <LoginForm />
+    <AuthPage />
+
   </div>
 </template>
 
 <script lang="ts">
 // import '../firebase/config'
-import SignupForm from '../components/login/SignupForm.vue';
-import LoginForm from '../components/login/LoginForm.vue';
+import AuthPage from 'src/components/login/AuthPage.vue';
 import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'IndexPage',
   components: {
-    SignupForm,
-    LoginForm,
+    AuthPage
   },
   methods: {},
 });
