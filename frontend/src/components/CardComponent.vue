f325fcdf796d3982590bcd7c99c574c4b9b1207d
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-04 10:24:47 +0800

    Add card for settings as items to carousel

---
+++ frontend/src/components/CardComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <q-card flat style="height: 320px; width: 250px;" class="transparent">
+      <img :src="url">
+
+      <q-card-actions align="center">
+        <ActionButton text-label="Play" />
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+
+<script>
+import ActionButton from './buttons/ActionButton.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CardComponent',
+  props: {
+    imageUrl: String,
+  },
+  components: { ActionButton },
+  setup(props) {
+    return {
+      url: props.imageUrl,
+    }
+  }
+})
+</script>
---
+++ frontend/src/components/SettingComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+     <q-carousel
+        swipeable
+        animated
+        navigation
+        v-model="slide"
+        infinite
+        class="transparent"
+      >
+        <q-carousel-slide :name="1">
+          <CardComponent imageUrl="/setting1.svg" />
+        </q-carousel-slide>
+
+        <q-carousel-slide :name="2">
+          <CardComponent imageUrl="/setting2.svg"  />
+        </q-carousel-slide>
+
+        <q-carousel-slide :name="3">
+          <CardComponent imageUrl="/setting3.svg"  />
+        </q-carousel-slide>
+
+        <q-carousel-slide :name="4">
+          <CardComponent imageUrl="/setting4.svg"  />
+        </q-carousel-slide>
+
+      </q-carousel>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import CardComponent from './CardComponent.vue';
+
+export default defineComponent({
+    name: 'SettingComponent',
+    props: {
+        imageUrl: String
+    },
+    setup(props) {
+        return {
+            image: props.imageUrl,
+            slide: ref(1),
+        };
+    },
+    components: { CardComponent }
+})
+</script>
