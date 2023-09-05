5b05bf7e980eb03adcf349a3a15420058f133652
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-06 02:52:51 +0800

    Modify size and margin to cater responsiveness

--- frontend/src/components/CardComponent.vue
+++ frontend/src/components/CardComponent.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <q-card flat style="height: 250px; width: 250px;" class="transparent">
+    <q-card flat style="height: 200px; width: 180px;" class="transparent q-mt-lg">
       <img :src="url">
 
-      <q-card-actions align="center">
+      <q-card-actions class="q-mt-md" align="center">
         <ActionButton text-label="Play" />
       </q-card-actions>
     </q-card>
