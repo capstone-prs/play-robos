25dda254e8593dc38cd6f410076887c09d0becb3
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-06 02:54:33 +0800

    Add level board component and asset

---
+++ frontend/src/components/LevelBoard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+      <img src="/level-board.svg" style="width: 80; height: 80px;" >
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LevelBoard',
+  props: {
+    levelNumber: String,
+    levelGoal: String,
+    levelReward: String
+  },
+  setup(props) {
+    return {
+      levNumber: props.levelNumber,
+      levGoal: props.levelGoal,
+      levRew: props.levelReward
+    }
+  }
+})
+</script>
+
+<style>
+@font-face {
+  font-family: futura;
+  src: url('/fonts/FuturaLT.woff');
+}
+
+.futura {
+  font-family: 'futura';
+  color: yellow;
+}
+
+.board {
+  z-index: 1;
+  position: relative;
+}
+
+.details {
+  z-index: 2;
+}
+
+</style>
