6946c9991cfd670559166b77f530e9b084b85b1f
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-06 02:52:18 +0800

    Increase size of modal

--- frontend/src/components/buttons/MenuButton.vue
+++ frontend/src/components/buttons/MenuButton.vue
@@ -4,41 +4,43 @@
       <q-icon name="img:/menu-btn.svg" size="50px" />
     </q-btn>
 
-    <q-dialog v-model="inception">
-        <q-card>
-          <q-card-section class="row items-center card-title" style="width: 400px;" >
-            <div class="text-h4 futura">Menu</div>
-            <q-space />
-            <q-btn icon="close" color="white" size="md" flat v-close-popup />
-          </q-card-section>
+      <q-dialog v-model="inception" style="background-color: transparent;">
+          <q-card>
+            <q-card-section class="row items-center card-title" style="width: 400px;" >
+              <div class="text-h4 futura">Menu</div>
+              <q-space />
+              <q-btn icon="close" color="white" size="md" flat v-close-popup />
+            </q-card-section>
 
-          <q-card-section class=" centered">
-            <toggle-button />
-          </q-card-section>
+            <q-card-section class=" centered">
+              <toggle-button />
+            </q-card-section>
 
-          <q-card-section class="q-pt-none centered">
-            <action-button text-label="Logout" @click="secondDialog = true" />
-          </q-card-section>
-        </q-card>
-    </q-dialog>
-
-    <q-dialog v-model="secondDialog" persistent transition-show="scale" transition-hide="scale">
-        <q-card class="bg-blue text-white" style="width: 300px" >
-          <q-card-section>
-            <div class="text-h6">We're sad to see you leave</div>
-          </q-card-section>
-
-          <q-card-section class="q-pt-none">
-            Are you sure you want to log out?
-          </q-card-section>
-
-          <q-card-actions align="center" class="bg-white text-teal">
-            <q-btn flat label="Cancel" class="text-blue" v-close-popup />
-            <q-btn flat label="Yes" class="text-pink"  v-close-popup />
-          </q-card-actions>
-
-        </q-card>
+            <q-card-section class="q-pt-none centered">
+              <action-button text-label="Logout" @click="secondDialog = true" />
+            </q-card-section>
+          </q-card>
       </q-dialog>
+
+      <q-dialog style="background-color: transparent;" v-model="secondDialog" persistent transition-show="scale" transition-hide="scale">
+          <q-card class="bg-blue text-white" style="width: 300px;" >
+
+            <q-card-section>
+              <div class="text-h6">We're sad to see you leave</div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+              Are you sure you want to log out?
+            </q-card-section>
+
+            <q-card-actions align="center" class="bg-white text-teal">
+              <q-btn flat label="Cancel" class="text-blue" v-close-popup />
+              <q-btn flat label="Yes" class="text-pink"  v-close-popup />
+            </q-card-actions>
+
+          </q-card>
+      </q-dialog>
+
 
   </div>
 </template>
