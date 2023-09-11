59ca43a60e5f5fdd752a35d872e5e1873a4a8059
RenzoLaporno <renzogabriel.laporno-19@cpu.edu.ph>
2023-09-12 01:23:57 +0800

    fix #25 added blockly blocks with their respective icons

--- frontend/src/components/blockly/toolbox/toolbox.js
+++ frontend/src/components/blockly/toolbox/toolbox.js
@@ -3,15 +3,103 @@ export const level1 = {
           'contents': [
             {
               'kind': 'block',
-              'type': 'wink'
+              'type': 'normal_look'
             },
             {
               'kind': 'block',
               'type': 'blink'
             },
-              {
+            {
               'kind': 'block',
-              'type': 'eye_roll'
+              'type': 'look_up'
+            },  
+            {
+              'kind': 'block',
+              'type': 'look_right'
+            },  
+            {
+              'kind': 'block',
+              'type': 'look_left'
+            }, 
+            {
+              'kind': 'block',
+              'type': 'look_up_left'
+            }, 
+            {
+              'kind': 'block',
+              'type': 'look_up_right'
+            }, 
+            {
+              'kind': 'block',
+              'type': 'wink_right'
+            },         
+            {
+              'kind': 'block',
+              'type': 'wink_left'
+            },  
+            {
+              'kind': 'block',
+              'type': 'close_eyes'
+            },  
+            {
+              'kind': 'block',
+              'type': 'turn_head_right'
+            },  
+            {
+              'kind': 'block',
+              'type': 'head_trun_left'
+            },  
+            
+
+            {
+              'kind': 'block',
+              'type': 'left_arm_raise'
+            },
+            {
+              'kind': 'block',
+              'type': 'right_arm_raise'
+            },
+            {
+              'kind': 'block',
+              'type': 'half_right_arm_raise'
+            },
+            {
+              'kind': 'block',
+              'type': 'half_left_arm_raise'
+            },
+            {
+              'kind': 'block',
+              'type': 'semi_close'
+            },
+
+            {
+              'kind': 'block',
+              'type': 'left_wheel_forward'
+            },
+            {
+              'kind': 'block',
+              'type': 'right_wheel_forward'
+            },
+            {
+              'kind': 'block',
+              'type': 'right_wheel_backward'
+            },
+            {
+              'kind': 'block',
+              'type': 'left_wheel_backward'
+            },
+            
+            {
+              'kind': 'block',
+              'type': 'time_block'
+            },
+            {
+              'kind': 'block',
+              'type': 'all_together'
+            },
+            {
+              'kind': 'block',
+              'type': 'all_parts'
             }
           ]
         }
