2a86d430bee0ba9a8e1e4acd653b12de9744544d
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-14 02:09:43 +0800

    Add upload button

--- frontend/src/components/blockly/BlocklyComponent.vue
+++ frontend/src/components/blockly/BlocklyComponent.vue
@@ -1,72 +1,81 @@
 <template>
   <div class="workspace-container">
-
     <div class="overlay-container">
       <div class="row">
-        <div class="col-6 check">
+        <div class="col-4 check">
           <ActionButton text-label="CHECK" data-testid="check-btn" />
         </div>
-        <div class="col-3 buttons" data-testid="help-btn">
+        <div class="col-4 check">
+          <ActionButton
+            text-label="UPLOAD"
+            data-testid="check-btn"
+            color="pink"
+            text-color="white"
+          />
+        </div>
+        <div class="col-2 buttons" data-testid="help-btn">
           <HelpButton />
         </div>
-        <div class="col-3 buttons" data-testid="menu-btn" >
+        <div class="col-2 buttons" data-testid="menu-btn">
           <MenuButton />
         </div>
       </div>
     </div>
 
-    <div ref="blocklyContainer" class="blockly-container" data-testid="blockly-container"></div>
-
+    <div
+      ref="blocklyContainer"
+      class="blockly-container"
+      data-testid="blockly-container"
+    ></div>
   </div>
-
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue';
 import Blockly from 'blockly';
-import './blocks/stocks'
-import { level1 } from './toolbox/toolbox'
+import './blocks/stocks';
+import { level1 } from './toolbox/toolbox';
 import MenuButton from '../buttons/MenuButton.vue';
 import HelpButton from '../buttons/HelpButton.vue';
 import ActionButton from '../buttons/ActionButton.vue';
 
 export default defineComponent({
-    name: 'BlocklyComponent',
-    setup() {
-        const blocklyContainer = ref<string | Element>('');
-        onMounted(() => {
-            Blockly.inject(blocklyContainer.value, {
-                // refer to toolbox.js file, we can define more levels from there,
-                // future handling may be passing the level number as props to this component
-                toolbox: level1,
-                trashcan: true,
-                grid: {
-                    spacing: 20,
-                    length: 3,
-                    colour: '#ccc',
-                    snap: true
-                },
-                zoom: {
-                    startScale: 1.5,
-                    maxScale: 3,
-                    minScale: 0.3,
-                    scaleSpeed: 1.2,
-                },
-                theme: {
-                    name: 'custom',
-                    'componentStyles': {
-                        'workspaceBackgroundColour': '#FFFFFF',
-                        'flyoutBackgroundColour': '#D0D0D0',
-                        'flyoutOpacity': 0.7,
-                    },
-                }
-            });
-        });
-        return {
-            blocklyContainer,
-        };
-    },
-    components: { MenuButton, HelpButton, ActionButton }
+  name: 'BlocklyComponent',
+  setup() {
+    const blocklyContainer = ref<string | Element>('');
+    onMounted(() => {
+      Blockly.inject(blocklyContainer.value, {
+        // refer to toolbox.js file, we can define more levels from there,
+        // future handling may be passing the level number as props to this component
+        toolbox: level1,
+        trashcan: true,
+        grid: {
+          spacing: 20,
+          length: 3,
+          colour: '#ccc',
+          snap: true,
+        },
+        zoom: {
+          startScale: 1.5,
+          maxScale: 3,
+          minScale: 0.3,
+          scaleSpeed: 1.2,
+        },
+        theme: {
+          name: 'custom',
+          componentStyles: {
+            workspaceBackgroundColour: '#FFFFFF',
+            flyoutBackgroundColour: '#D0D0D0',
+            flyoutOpacity: 0.7,
+          },
+        },
+      });
+    });
+    return {
+      blocklyContainer,
+    };
+  },
+  components: { MenuButton, HelpButton, ActionButton },
 });
 </script>
 
@@ -85,7 +94,7 @@ export default defineComponent({
   position: absolute;
   z-index: 2;
   padding-top: 5px;
-  left: 68%;
+  left: 55%;
 }
 
 .buttons {
@@ -96,4 +105,3 @@ export default defineComponent({
   padding: 5px;
 }
 </style>
-
