85ada5e043b506b5427f361ca68f67bba4a2db25
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-19 01:15:51 +0800

    Widen workspace

--- frontend/src/components/blockly/BlocklyComponent.vue
+++ frontend/src/components/blockly/BlocklyComponent.vue
@@ -11,7 +11,7 @@
           <CheckDialog v-model="isDialogOpen" data-testid="check-dialog" />
         </div>
 
-        <div class="col-3 buttons" data-testid="help-btn">
+        <div class="col-3 buttons" data-testid="help-btn" @click="showCode">
           <HelpButton />
         </div>
         <div class="col-3 buttons" data-testid="menu-btn">
@@ -28,65 +28,63 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
-import Blockly from 'blockly';
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import * as Blockly from 'blockly';
 import './blocks/stocks';
+import './blocks/generator';
+
 import { level1 } from './toolbox/toolbox';
 import MenuButton from '../buttons/MenuButton.vue';
 import HelpButton from '../buttons/HelpButton.vue';
 import ActionButton from '../buttons/ActionButton.vue';
 import CheckDialog from '../CheckDialog.vue';
+import { javascriptGenerator } from 'blockly/javascript';
 
-export default defineComponent({
-  name: 'BlocklyComponent',
-  setup() {
-    const isDialogOpen = ref(false);
-    const openUploadDialog = () => {
-      isDialogOpen.value = true;
-    };
-    const blocklyContainer = ref<string | Element>('');
-    onMounted(() => {
-      Blockly.inject(blocklyContainer.value, {
-        // refer to toolbox.js file, we can define more levels from there,
-        // future handling may be passing the level number as props to this component
-        toolbox: level1,
-        trashcan: true,
-        grid: {
-          spacing: 20,
-          length: 3,
-          colour: '#ccc',
-          snap: true,
-        },
-        zoom: {
-          startScale: 1.3,
-          maxScale: 3,
-          minScale: 0.3,
-          scaleSpeed: 1.0,
-        },
-        theme: {
-          name: 'custom',
-          componentStyles: {
-            workspaceBackgroundColour: '#FFFFFF',
-            flyoutBackgroundColour: '#D0D0D0',
-            flyoutOpacity: 0.7,
-          },
-        },
-      });
-    });
-    return {
-      blocklyContainer,
-      openUploadDialog,
-      isDialogOpen,
-    };
-  },
-  components: {
-    MenuButton,
-    HelpButton,
-    ActionButton,
-    CheckDialog,
-  },
+const isDialogOpen = ref(false);
+const openUploadDialog = () => {
+  isDialogOpen.value = true;
+};
+
+let generator: any = '';
+
+const blocklyContainer = ref<string | Element>('');
+onMounted(() => {
+  let workspace = Blockly.inject(blocklyContainer.value, {
+    // refer to toolbox.js file, we can define more levels from there,
+    // future handling may be passing the level number as props to this component
+    toolbox: level1,
+    trashcan: true,
+    grid: {
+      spacing: 20,
+      length: 3,
+      colour: '#ccc',
+    },
+    zoom: {
+      startScale: 1.3,
+      maxScale: 3,
+      minScale: 0.3,
+      scaleSpeed: 0.3,
+    },
+    theme: {
+      name: 'custom',
+      componentStyles: {
+        workspaceBackgroundColour: '#FFFFFF',
+        flyoutBackgroundColour: '#D0D0D0',
+        flyoutOpacity: 0.7,
+      },
+    },
+  });
+
+  generator = () => {
+    return javascriptGenerator.workspaceToCode(workspace as Blockly.Workspace);
+  };
+  workspace.addChangeListener(generator);
 });
+
+const showCode = () => {
+  console.log(generator());
+};
 </script>
 
 <style>
