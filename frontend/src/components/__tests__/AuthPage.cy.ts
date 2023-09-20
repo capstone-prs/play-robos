fdd666a5e964eba9696885bea5c657b370943bcf
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-09-21 01:14:54 +0800

    Add some component test for Auth page

---
+++ frontend/src/components/__tests__/AuthPage.cy.ts
@@ -0,0 +1,21 @@
+import AuthPage from '../login/AuthPage.vue';
+
+describe('Authentication Page', () => {
+  beforeEach(() => {
+    cy.mount(AuthPage);
+  });
+  it('renders the background', () => {
+    cy.get('img')
+      .should('exist')
+      .should('have.attr', 'src', '/__cypress/src/src/assets/PlayRobos1.svg');
+  });
+
+  it('renders the game title', () => {
+    cy.get('h2').should('exist').should('contain.text', 'PlayRoboS');
+  });
+
+  it('should render login and signup buttons', () => {
+    cy.get('[data-testid="login-btn"]').should('exist');
+    cy.get('[data-testid="signup-btn"]').should('exist');
+  });
+});
