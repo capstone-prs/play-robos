a814b3a613006e58890e103f3a3c6b556c8d2ead
RenzoLaporno <renzogabriel.laporno-19@cpu.edu.ph>
2023-10-10 02:43:28 +0800

    UI enhancments

--- frontend/src/components/blockly/BlocklyComponent.vue
+++ frontend/src/components/blockly/BlocklyComponent.vue
@@ -25,7 +25,7 @@
         </div>
         <div class="col-2 buttons" data-testid="menu-btn">
           <MenuButton @click="openMenuDialog"/>
-          <MenuActivityDialog v-model="showMenuActivity"/>
+          <MenuDialog v-model="showMenuActivity"/>
         </div>
       </div>
       <div class="row">
@@ -62,7 +62,7 @@ import { javascriptGenerator } from 'blockly/javascript';
 import UndoButton from '../buttons/UndoButton.vue';
 import { useRouter } from 'vue-router';
 import { bluetoothWrite, bluetoothSerial } from 'src/utils/bluetoothUtils';
-import MenuActivityDialog from '../../components/MenuActivityDialog.vue'
+import MenuDialog from '../../components/MenuDialog.vue'
 
 const route = useRouter().currentRoute;
 const routeParam = route.value.params.param as string;
