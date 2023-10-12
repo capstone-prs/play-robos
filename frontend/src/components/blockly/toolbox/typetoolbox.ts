5d0b390cd8f632946a5f484d0874840ab26f4211
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-10-12 13:27:45 +0800

    Add toolbox for age group 8-11

--- frontend/src/components/blockly/toolbox/typetoolbox.ts
+++ frontend/src/components/blockly/toolbox/typetoolbox.ts
@@ -1,41 +1,5 @@
 import { ToolboxDefinition } from 'blockly/core/utils/toolbox';
 
-// define blocks for each setting. NOTE: To-change based on activities
-const setting1 = [
-  'normal_look',
-  'close_eyes',
-  'look_right',
-  'look_left',
-  'look_up_left',
-  'look_up_right',
-];
-
-const setting2 = [
-  'normal_look',
-  'look_up',
-  'look_right',
-  'look_left',
-  'look_up_left',
-  'look_up_right',
-  'wink_right',
-  'wink_left',
-  'semi_close',
-  'close_eyes',
-];
-
-const setting3 = [
-  'normal_look',
-  'look_up',
-  'look_right',
-  'look_left',
-  'look_up_left',
-  'look_up_right',
-  'wink_right',
-  'wink_left',
-  'semi_close',
-  'close_eyes',
-];
-
 // get the blocks of a certain setting
 const getBlocks = (setting: Array<string>) => {
   return setting.map((block) => {
@@ -46,20 +10,104 @@ const getBlocks = (setting: Array<string>) => {
   });
 };
 
+// define blocks for each setting
+const setting1_5_7 = [
+  'normal_look',
+  'close_eyes',
+  'look_right',
+  'look_left',
+  'look_up_left',
+  'look_up_right',
+];
+
+const setting2_5_7 = [
+  'normal_look',
+  'look_up',
+  'look_right',
+  'look_left',
+  'look_up_left',
+  'look_up_right',
+  'wink_right',
+  'wink_left',
+  'semi_close',
+  'close_eyes',
+];
+
+const setting3_5_7 = [
+  'normal_look',
+  'look_up',
+  'look_right',
+  'look_left',
+  'look_up_left',
+  'look_up_right',
+  'wink_right',
+  'wink_left',
+  'semi_close',
+  'close_eyes',
+];
+
 // export the toolbox of each setting
 export const toolbox_5_7: Array<ToolboxDefinition> = [
   {
     kind: 'flyoutToolbox',
-    contents: getBlocks(setting1),
+    contents: getBlocks(setting1_5_7),
   },
   {
     kind: 'flyoutToolbox',
-    contents: getBlocks(setting2),
+    contents: getBlocks(setting2_5_7),
   },
   {
     kind: 'flyoutToolbox',
-    contents: getBlocks(setting3),
+    contents: getBlocks(setting3_5_7),
   },
 ];
 
-export const toolbox_8_11: Array<ToolboxDefinition> = [];
+const setting1_5_8 = [
+  'normal_look',
+  'close_eyes',
+  'look_right',
+  'look_left',
+  'look_up_left',
+  'look_up_right',
+];
+
+const setting2_5_8 = [
+  'normal_look',
+  'look_up',
+  'look_right',
+  'look_left',
+  'look_up_left',
+  'look_up_right',
+  'wink_right',
+  'wink_left',
+  'semi_close',
+  'close_eyes',
+];
+
+const setting3_5_8 = [
+  'normal_look',
+  'look_up',
+  'look_right',
+  'look_left',
+  'look_up_left',
+  'look_up_right',
+  'wink_right',
+  'wink_left',
+  'semi_close',
+  'close_eyes',
+];
+
+export const toolbox_8_11: Array<ToolboxDefinition> = [
+  {
+    kind: 'flyoutToolbox',
+    contents: getBlocks(setting1_5_8),
+  },
+  {
+    kind: 'flyoutToolbox',
+    contents: getBlocks(setting2_5_8),
+  },
+  {
+    kind: 'flyoutToolbox',
+    contents: getBlocks(setting3_5_8),
+  },
+];
