63a0c62305d2d0ebb29a5e09ebf8581814ce79ff
RenzoLaporno <renzogabriel.laporno-19@cpu.edu.ph>
2023-11-18 04:47:50 +0800

    Add Categories for blockly toolbox

--- frontend/src/components/blockly/toolbox/typetoolbox.ts
+++ frontend/src/components/blockly/toolbox/typetoolbox.ts
@@ -5,126 +5,242 @@ const getBlocks = (setting: Array<string>) => {
   return setting.map((block) => {
     return {
       kind: 'block',
-      type: block,
+      type: block
     };
   });
 };
 
 // define blocks for each setting
-const setting1_5_7 = [
+//<-------- setting 1 ---------------->
+const setting1_5_7_eyes = [
   'normal_look',
   'close_eyes',
   'look_right',
   'look_left',
   'look_up_left',
-  'look_up_right',
-  'raise_left_arm_60_',
-  'raise_right_arm_60_',
+  'look_up_right'
 ];
+const setting1_5_7_arms = ['raise_left_arm_60_', 'raise_right_arm_60_'];
+//<-------- setting 1 ---------------->
 
-const setting2_5_7 = [
+//-----------------------------------//
+
+//<-------- setting 2 ---------------->
+const setting2_5_7_eyes = [
   'normal_look',
   'close_eyes',
   'look_right',
   'look_left',
   'look_up_left',
-  'look_up_right',
-  'raise_left_arm_60_',
-  'raise_right_arm_60_',
-  'head_trun_left',
-  'turn_head_right',
+  'look_up_right'
 ];
+const setting2_5_7_arms = ['raise_left_arm_60_', 'raise_right_arm_60_'];
 
-const setting3_5_7 = [
+const setting2_5_7_head = ['head_trun_left', 'turn_head_right'];
+//<-------- setting 2 ---------------->
+
+//-----------------------------------//
+
+//<-------- setting 3 ---------------->
+const setting3_5_7_eyes = [
   'normal_look',
   'close_eyes',
   'look_right',
   'look_left',
   'look_up_left',
-  'look_up_right',
-  'head_trun_left',
-  'turn_head_right',
+  'look_up_right'
+];
+const setting3_5_7_arms = [
   'raise_left_arm_60_',
   'raise_right_arm_60_',
   'raise_left_arm_90_',
-  'raise_right_arm_90_',
+  'raise_right_arm_90_'
 ];
+
+const setting3_5_7_head = ['head_trun_left', 'turn_head_right'];
+//<-------- setting 3 ---------------->
 
 // export the toolbox of each setting. NOTE: To improve
 export const toolbox_5_7: Array<ToolboxDefinition> = [
+  //<-------- setting 1 ---------------->
   {
-    kind: 'flyoutToolbox',
-    contents: getBlocks(setting1_5_7),
+    kind: 'categoryToolbox',
+    contents: [
+      {
+        kind: 'category',
+        name: 'Eyes',
+        contents: getBlocks(setting1_5_7_eyes)
+      },
+      {
+        kind: 'category',
+        name: 'Arms',
+        contents: getBlocks(setting1_5_7_arms)
+      }
+    ]
   },
+  //<-------- setting 1 ---------------->
+  //-----------------------------------//
+  //<-------- setting 2 ---------------->
   {
-    kind: 'flyoutToolbox',
-    contents: getBlocks(setting2_5_7),
+    kind: 'categoryToolbox',
+    contents: [
+      {
+        kind: 'category',
+        name: 'Head',
+        contents: getBlocks(setting2_5_7_head)
+      },
+      {
+        kind: 'category',
+        name: 'Eyes',
+        contents: getBlocks(setting2_5_7_eyes)
+      },
+      {
+        kind: 'category',
+        name: 'Arms',
+        contents: getBlocks(setting2_5_7_arms)
+      }
+    ]
   },
+  //<-------- setting 2 ---------------->
+  //-----------------------------------//
+  //<-------- setting 2 ---------------->
   {
-    kind: 'flyoutToolbox',
-    contents: getBlocks(setting3_5_7),
-  },
+    kind: 'categoryToolbox',
+    contents: [
+      {
+        kind: 'category',
+        name: 'Head',
+        contents: getBlocks(setting3_5_7_head)
+      },
+      {
+        kind: 'category',
+        name: 'Eyes',
+        contents: getBlocks(setting3_5_7_eyes)
+      },
+      {
+        kind: 'category',
+        name: 'Arms',
+        contents: getBlocks(setting3_5_7_arms)
+      }
+    ]
+  }
 ];
-
-const setting1_5_8 = [
+//<-------- setting 1 age group8-11---------------->
+const setting1_5_8_eyes = [
   'normal_look',
   'close_eyes',
   'look_right',
   'look_left',
   'look_up_left',
-  'look_up_right',
-  'head_trun_left',
-  'turn_head_right',
+  'look_up_right'
+];
+const setting1_5_8_head = ['head_trun_left', 'turn_head_right'];
+const setting1_5_8_arms = [
   'raise_left_arm_120',
   'raise_right_arm_120_',
   'raise_right_arm_60_',
-  'raise_left_arm_60_',
+  'raise_left_arm_60_'
 ];
+//<-------- setting 1 age group8-11---------------->
 
-const setting2_5_8 = [
+const setting2_5_8_eyes = [
   'normal_look',
   'close_eyes',
   'look_right',
   'look_left',
   'look_up_left',
-  'look_up_right',
-  'head_trun_left',
-  'turn_head_right',
+  'look_up_right'
+];
+const setting2_5_8_head = ['head_trun_left', 'turn_head_right'];
+const setting2_5_8_arms = [
   'raise_left_arm_120',
   'raise_right_arm_120_',
   'raise_left_arm_90_',
-  'raise_right_arm_90_',
+  'raise_right_arm_90_'
 ];
 
-const setting3_5_8 = [
+const setting3_5_8_eyes = [
   'normal_look',
   'close_eyes',
   'look_right',
   'look_left',
   'look_up_left',
-  'look_up_right',
+  'look_up_right'
+
+];
+const setting3_5_8_head = [
   'head_trun_left',
-  'turn_head_right',
+  'turn_head_right'
+];
+const setting3_5_8_arms = [
   'raise_left_arm_120',
   'raise_right_arm_120_',
   'raise_left_arm_90_',
   'raise_right_arm_90_',
   'raise_right_arm_150_',
-  'raise_left_arm_150',
+  'raise_left_arm_150'
 ];
 
 // NOTE: To improve
 export const toolbox_8_11: Array<ToolboxDefinition> = [
   {
-    kind: 'flyoutToolbox',
-    contents: getBlocks(setting1_5_8),
+    kind: 'categoryToolbox',
+    contents: [
+      {
+        kind: 'category',
+        name: 'Head',
+        contents: getBlocks(setting1_5_8_head)
+      },
+      {
+        kind: 'category',
+        name: 'Eyes',
+        contents: getBlocks(setting1_5_8_eyes)
+      },
+      {
+        kind: 'category',
+        name: 'Arms',
+        contents: getBlocks(setting1_5_8_arms)
+      }
+    ]
   },
   {
-    kind: 'flyoutToolbox',
-    contents: getBlocks(setting2_5_8),
+    kind: 'categoryToolbox',
+    contents: [
+      {
+        kind: 'category',
+        name: 'Head',
+        contents: getBlocks(setting2_5_8_head)
+      },
+      {
+        kind: 'category',
+        name: 'Eyes',
+        contents: getBlocks(setting2_5_8_eyes)
+      },
+      {
+        kind: 'category',
+        name: 'Arms',
+        contents: getBlocks(setting2_5_8_arms)
+      }
+    ]
   },
   {
-    kind: 'flyoutToolbox',
-    contents: getBlocks(setting3_5_8),
-  },
+    kind: 'categoryToolbox',
+    contents: [
+      {
+        kind: 'category',
+        name: 'Head',
+        contents: getBlocks(setting3_5_8_head)
+      },
+      {
+        kind: 'category',
+        name: 'Eyes',
+        contents: getBlocks(setting3_5_8_eyes)
+      },
+      {
+        kind: 'category',
+        name: 'Arms',
+        contents: getBlocks(setting3_5_8_arms)
+      }
+    ]
+  }
 ];
