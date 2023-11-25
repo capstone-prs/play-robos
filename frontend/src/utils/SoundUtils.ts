f05a633c3a10f05a8d5633e21594890ef8c14f3e
RenzoLaporno <renzogabriel.laporno-19@cpu.edu.ph>
2023-11-25 20:10:56 +0800

    Add music background function on utils

--- frontend/src/utils/SoundUtils.ts
+++ frontend/src/utils/SoundUtils.ts
@@ -1,6 +1,7 @@
 import click from 'assets/sounds/click.mp3'
 import { Howl} from 'howler';
 import background from '../assets/sounds/background-music-2.mp3'
+import backgroundSudio from '../assets/sounds/background-music-3.mp3'
 export const soundEffect = (sound:string=click)=>{
     const toPLay = new Howl({src:[sound]})
     toPLay.load();
@@ -12,4 +13,11 @@ export const backgroundMusic = new Howl({
     loop: true, // Set to true for continuous looping
     volume: 1 // Adjust the volume as needed
   });
+
+export const backgroundMusicStudio = new Howl({
+  src: [backgroundSudio],
+  loop: true, // Set to true for continuous looping
+  volume: 1 // Adjust the volume as needed
+})
+
 
